baec8aa9accd4a7143bc88afd0a420faeaf500f0
Mariana Oliveira <olvr.mariana@gmail.com>
2020-12-28 23:26:48 -0300

    add cors module

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const list = [];
