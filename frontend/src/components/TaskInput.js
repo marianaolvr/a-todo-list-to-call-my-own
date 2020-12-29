a42fdc3303137241aab22cf43b26c92f7759beac
Mariana Oliveira <olvr.mariana@gmail.com>
2020-12-29 00:11:06 -0300

    add task and update the list

--- frontend/src/components/TaskInput.js
+++ frontend/src/components/TaskInput.js
@@ -1,25 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import api from '../services/api'
+import api from "../services/api";
 
 export default function TaskInput() {
+  const [list, setList] = useState([]);
+  const [taskDescription, setTaskDescription ] = useState('');
+  const [author, setAuthor] = useState('');
 
-    return (
-        <>
-        <div className="card">
-            <p>Fazer xixi</p>
-            <p><b>Mariana</b></p>
-        </div>
-        <div className="input">
-            <label>Task</label>
-            <input type="text"/>
+  useEffect(() => {
+    api.get("/").then((response) => {
+      setList(response.data);
+    });
+  }, []);
 
-            <label>Author</label>
-            <input type="text"/>
 
-            <button>Adicionar</button>
-        </div>
+  async function handleAddTask(e){
 
-        </>
-    )
+    const data = {
+      taskDescription,
+      author
+    }
+
+    const response = await api.post('/', data)
+
+    const task = response.data;
+
+    setList([...list, task])
+  }
+
+  return (
+    <>
+      <div className="card">
+        {list.map((task) => (
+          <>
+            <p>{task.taskDescription}</p>
+            <p><b>{task.author}</b></p>
+          </>
+        ))}
+      </div>
+      <div className="input">
+        <label>Task: 
+          <input type="text" onChange={e => setTaskDescription(e.target.value)} />
+        </label>
+
+        <label>Author: 
+          <input type="text" onChange={e => setAuthor(e.target.value)} />
+        </label>
+
+        <button type="button" onClick={handleAddTask}>Adicionar</button>
+      </div>
+    </>
+  );
 }
