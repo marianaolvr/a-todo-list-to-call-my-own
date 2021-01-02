139f98fe968a2ee0ef2a7d6669dd5d5626e69326
Mariana Oliveira <olvr.mariana@gmail.com>
2021-01-01 21:54:15 -0300

    add style and css loaders

--- frontend/src/components/TaskInput.js
+++ frontend/src/components/TaskInput.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import api from "../services/api";
+import './TaskInput.css';
 
 export default function TaskInput() {
   const [list, setList] = useState([]);
